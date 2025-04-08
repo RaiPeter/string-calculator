dc357deccfbdb1c8bdbb9dfd3e8d1408d2a57f03
Peter Rai <rai.peter2@gmail.com>
2025-04-08 22:33:13 +0530

    added a failed test for a single argument as there's no support for it on the main function yet

--- calculator.test.js
+++ calculator.test.js
@@ -4,4 +4,7 @@ describe("Calculator", () => {
     test("empty string should return 0", ()=>{
         expect(calculator("")).toEqual(0);
     })
+    test("a single value should return as itself", ()=>{
+        expect(calculator("1").toEqual(1))
+    })
 })
