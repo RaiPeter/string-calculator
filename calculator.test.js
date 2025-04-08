66a89c03e7f813412733fe5bf930fb978ddf9594
Peter Rai <rai.peter2@gmail.com>
2025-04-09 01:16:13 +0530

    refactored the code by adding  logger method to show the test dynamic arguments on the test suite, so we know what data were passed on the logs

--- calculator.test.js
+++ calculator.test.js
@@ -2,56 +2,42 @@ const add = require("./calculator.js");
 
 describe("Calculator", () => {
   describe("empty string should return 0", () => {
-    it("empty string should return 0", () => {
-      expect(add("")).toEqual(0);
-    });
+    logger("", 0);
   });
 
   describe("a single value should return as itself", () => {
-    it("should return as 1", () => {
-      expect(add("1")).toEqual(1);
-    });
-    it("should return as 2", () => {
-      expect(add("2")).toEqual(2);
-    });
+    logger("1", 1);
+    logger("2", 2);
   });
 
   describe("comma separator - multiple values should return the sum of it", () => {
-    it("should return as 3", () => {
-      expect(add("1,2")).toEqual(3);
-    });
-    it("should return as 6", () => {
-      expect(add("1,2,3")).toEqual(6);
-    });
-    it("should return as 10", () => {
-      expect(add("1,2,3,4")).toEqual(10);
-    });
+    logger("1,2", 3);
+    logger("1,2,3", 6);
+    logger("1,2,3,4", 10);
   });
 
   describe("newline separator", () => {
-    it("should return as 6", () => {
-      expect(add("1\n2,3")).toEqual(6);
-    });
-    it("should return as 25", () => {
-      expect(add("1\n2,3\n4\n5\n6,7")).toEqual(28);
-    });
+    logger("1\n2,3", 6);
+    logger("1\n2,3\n4\n5\n6,7",28)
   });
 
   describe("different delimeters", () => {
-    it("should return as 3", () => {
-      expect(add("//;\n1;2")).toEqual(3);
-    });
-    it("should return as 3", () => {
-      expect(add("//&\n1&2&3&4&5")).toEqual(15);
-    });
+    logger("//;\n1;2", 3)
+    logger("//&\n1&2&3&4&5",15)
   });
 
   describe("no negative numbers allowed", () => {
-    it("should throw an error", () => {
+    it("-1 should throw an error", () => {
       expect(() => add("-1")).toThrow("negative numbers not allowed -1");
     });
-    it("should throw an error", () => {
+    it("-1,2,-3 should throw an error", () => {
       expect(() => add("-1,2,-3")).toThrow("negative numbers not allowed -1, -3");
     });
   });
+
+  function logger(operands, sum){
+    it(`should return "${operands.replace(/\n/g,'\\n')}" as ${sum}`, () => {
+      expect(add(operands)).toEqual(sum);
+    })
+  }
 });
